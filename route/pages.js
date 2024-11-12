f3c9e85033a7eab57dfbbb703f8d0a39e252e9f9
MdShadil07 <mdshadil62@gmail.com>
2024-11-12 18:19:25 +0530

    essential file for redirecting pages

--- route/pages.js
+++ route/pages.js
@@ -7,8 +7,17 @@ router.get('/', (req, res) => {
   res.render('dashboard')
 });
 
+
 router.get('/signup', (req, res) => {
   res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.get('/dashboard', (req, res)=>{
+  res.render('dashboard');
 });
 
 module.exports = router;
