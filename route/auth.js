400f61b69a66a948386e64cce4f6853fef2cf88b
MdShadil07 <mdshadil62@gmail.com>
2024-11-12 18:16:31 +0530

    authetication for both the login signup

--- route/auth.js
+++ route/auth.js
@@ -3,7 +3,9 @@ const authConroller = require('../controllers/auth');
 
 const router = express.Router();
 
-router.post('/signup', authConroller.signup )
+router.post('/signup', authConroller.signup );
+
+router.post('/login', authConroller.login);
 
 
 
